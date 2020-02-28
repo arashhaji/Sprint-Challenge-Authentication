8b30d727dd0f2a6cc032d094355d3d44b27f2fce
arashhaji <56742302+arashhaji@users.noreply.github.com>
2020-02-28 13:57:03 -0500

    mvp

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,15 @@
+// const knex = require('knex');
+
+// const knexConfig = require('../knexfile.js');
+
+// module.exports = knex(knexConfig.development);
+
 const knex = require('knex');
-
-const knexConfig = require('../knexfile.js');
-
-module.exports = knex(knexConfig.development);
+const config = require('../knexfile.js');
+// if the environment variable is not set, default to 'development'
+// this variable is only set when running the "test" npm script using npm run test
+const dbEnv = process.env.DB_ENV || 'development';
+// the value of dbEnv will be either 'development' or 'testing'
+// we pass it within brackets to select the corresponding configuration
+// from knexfile.js
+module.exports = knex(config[dbEnv]);
